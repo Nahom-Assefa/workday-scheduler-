8e1d1f7c2b5e69c2dc556f816015af5991eaf4a6
Nahom Assefa <nzenebe@gmail.com>
2021-11-13 11:48:05 -0600

    solved issue with color display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,62 @@
-// Time relative to the start of day
-var start = moment().startOf('day').fromNow();
-console.log(start);
 // Get current number of hours
-var rightNow = moment().format("- hh:mm:ss a");
+var rightNow = moment().format("HH");
 console.log(rightNow);
 // Specifying the date and time
 currentTime = moment();
-$("#currentDay").html(`Today's date is: ${currentTime.format("MMMM Do, YYYY")}`);
-// Array of timeBlocks
-var arr = [9,10,11,12,1,2,3,4,5];
+$("#currentDay").html(
+  `Today's date is: ${currentTime.format("MMMM Do, YYYY")}`
+);
+// Retrieve Id attribute from parent
+id = $(".saveBtn").parent().attr("id").replace("hour-", "");
 
 // Function handler
-$(document).ready(function(){
-// SaveBtn click listener
-$(".saveBtn").on("click", function(){
-// Store values from within the content-box
-textEl= $(".content-box").val();
-// Retrieve Id attribute from parent
-id = $(".saveBtn").parent().attr("id").replace("hour-","");
-console.log(id);
-// Set the local storage
-localStorage.setItem("text", textEl);
-localStorage.setItem("id", id);
+$(document).ready(function () {
+  // SaveBtn click listener
+  $(".saveBtn").on("click", function () {
+    // Store values from within the content-box
+    textEl = $(".content-box").val();
+    // Set the local storage
+    localStorage.setItem("text", textEl);
+    localStorage.setItem("id", id);
+  });
+});
 
-timeBlocks(id);
-})
-})
+var arr = document.querySelectorAll(".time-block");
+console.log(arr);
 
-// capture time in moment js in military time
-// compare ID with military time 
-// document.queryselectorall to pull down html elements
-
-/* loop over time blocks
-function timeBlocks(id) {
-for (i = id; i < arr.length; i++ ){
-    if (id < arr && id )
+// loop over time blocks
+function timeBlocks() {
+  arr.forEach(function (item) {
+    //console.log(item);
+    var textArea = item.querySelector(".content-box");
+    //console.log(textArea);
+    //retrieving id in loop to compare with military time
+    var ids = item.getAttribute("id").replace("hour-", "");
+    //console.log(ids);
+    if (ids < moment().format("HH")) {
+      $(`#${item.getAttribute("id")} .content-box`).addClass("past");
+      console.log(
+        $(`#${item.getAttribute("id")} .content-box`).addClass("past")
+      );
+    } else if (ids === moment().format("HH")) {
+      $(textArea).addClass("present");
+      //console.log(textArea);
+    } else {
+      $(textArea).addClass("future");
+      //console.log($(textArea));
+    }
+  });
 }
 
+timeBlocks();
 
-
-}*/
-
-
-
-
-// To check the time and add the classes for background indicators
-
-
-
-
-
-
-
-   // Get item from local storage if any
-
-
-
-
-
+// Get item from local storage if any //
+$("#hour-9 .content-box").val(localStorage.getItem("hour-9"));
+$("#hour-10 .content-box").val(localStorage.getItem("hour-10"));
+$("#hour-11 .content-box").val(localStorage.getItem("hour-11"));
+$("#hour-12 .content-box").val(localStorage.getItem("hour-12"));
+$("#hour-13 .content-box").val(localStorage.getItem("hour-13"));
+$("#hour-14 .content-box").val(localStorage.getItem("hour-14"));
+$("#hour-15 .content-box").val(localStorage.getItem("hour-15"));
+$("#hour-16 .content-box").val(localStorage.getItem("hour-16"));
+$("#hour-17 .content-box").val(localStorage.getItem("hour-17"));
